5cb5cb0327e06942a1310428147cd8ffcf59ae58
SAHIL000002 <asahilanshari2507@gmail.com>
2026-07-07 19:33:17 +0530

    nodejs basics and cover HTTP methods

--- node JS/server.js
+++ node JS/server.js
@@ -1,1 +1,13 @@
-console.log("server is running")
+const express = require('express'); // require load the package express 
+
+const app =  express() // her we create a instance of a server and store in app variable
+
+app.get("/",(req , res)=>{  // req is used for access the data comming from frontend 
+    res.send("Hello World")   // res is used for send data to backend 
+})
+
+app.get("/contact" , (req ,res)=>{
+    res.send("contact me at 9:00 AM")
+})
+
+app.listen(3000) // listen server ko start krne ke liye -> server ka instance to uper hi bn gya h but start nhi hua tha .
